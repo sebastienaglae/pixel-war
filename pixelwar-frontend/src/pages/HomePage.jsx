1c3d8b3c56274d8aaf4663807695e76b25725fc5
Sébastien Aglaé <38996444+sebastienaglae@users.noreply.github.com>
2024-02-26 16:10:35 +0100

    feat(front): adding basic router and react strap

---
+++ pixelwar-frontend/src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import { Button } from "reactstrap";
+
+function HomePage() {
+    return (  
+        <div>
+            <h1>Home Page</h1>
+            <Button color="primary">Primary</Button>
+        </div>
+    );
+}
+
+export default HomePage;
---
+++ pixelwar-frontend/src/pages/TestPage.jsx
@@ -0,0 +1,12 @@
+import { Button } from "reactstrap";
+
+function TestPage() {
+    return (
+        <div>
+            <h1>Test Page</h1>
+            <Button color="primary">Primary</Button>
+        </div>
+    );
+}
+
+export default TestPage;
