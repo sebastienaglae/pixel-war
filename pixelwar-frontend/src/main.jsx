cb6e55c72ac5c7f74b19fc947fff568018898c63
Sébastien Aglaé <38996444+sebastienaglae@users.noreply.github.com>
2024-03-30 18:06:51 +0100

    feat(front): add reddit theme

--- pixelwar-frontend/src/main.jsx
+++ pixelwar-frontend/src/main.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "./index.css";
+// import "./index.css";
+import "./index.scss";
 import { BrowserRouter } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
 import "react-datetime-picker/dist/DateTimePicker.css";
 import "react-calendar/dist/Calendar.css";
 import "react-clock/dist/Clock.css";
