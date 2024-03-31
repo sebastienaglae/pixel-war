147c57ffaf7f8693e762e04584a6b06b7368c2da
Sébastien Aglaé <38996444+sebastienaglae@users.noreply.github.com>
2024-03-31 21:11:59 +0200

    feat(front): Colors can be removed and are correctly saved

--- pixelwar-frontend/src/components/admin/ColorItem.jsx
+++ pixelwar-frontend/src/components/admin/ColorItem.jsx
@@ -2,7 +2,7 @@
 import { Input, Button } from "reactstrap";
 import { FaTrash } from "react-icons/fa";
 
-function ColorItem({ id, onColorChange }) {
+function ColorItem({ id, onColorChange, value, onDelete }) {
   return (
     <div className='d-flex flex-row align-items-center'>
       <p className='m-2'>#{id}</p>
@@ -10,8 +10,9 @@ function ColorItem({ id, onColorChange }) {
         type='color'
         className='p-0 m-2'
         onChange={(e) => onColorChange(id, e.target.value)}
+        value={value}
       />
-      <Button color='danger'>
+      <Button color='danger' onClick={() => onDelete(id)}>
         <FaTrash />
       </Button>
     </div>
