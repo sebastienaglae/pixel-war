986cc2854182896dc1ee0c7ab135145d720e336b
Sébastien Aglaé <38996444+sebastienaglae@users.noreply.github.com>
2024-03-31 20:42:19 +0200

    feat(front): Add colors in create/edit board

--- pixelwar-frontend/src/pages/admin/CreateBoardPage.jsx
+++ pixelwar-frontend/src/pages/admin/CreateBoardPage.jsx
@@ -1,15 +1,17 @@
-import React from "react";
+import { useState } from "react";
 import PixelBoardForm from "@components/admin/PixelBoardForm";
 
 function CreateBoardPage() {
+  const [error, setError] = useState("");
   const handleSubmit = (formData) => {
     console.log("Création avec :", formData);
+    setError("Une erreur s'est produite");
   };
 
   return (
-    <div className="mt-5">
+    <div className='mt-5'>
       <h2 className='text-center mb-5'>Créer un nouveau tableau</h2>
-      <PixelBoardForm initialData={{}} onSubmit={handleSubmit} />
+      <PixelBoardForm initialData={{}} onSubmit={handleSubmit} error={error} />
     </div>
   );
 }
