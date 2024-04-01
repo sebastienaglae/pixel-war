a9f32b1010daf9e3e951513adc4b656a7cf8caa8
hunnybunnyv <valeriyaa.lapshina@gmail.com>
2024-04-01 21:02:28 +0200

    feat(front): HomePage initialization

--- pixelwar-frontend/src/components/common/NavigationBar.jsx
+++ pixelwar-frontend/src/components/common/NavigationBar.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 import { Navbar, NavbarBrand, Nav, NavItem, NavLink } from "reactstrap";
+<<<<<<< HEAD
 import { FaHome, FaRedditAlien, FaSignInAlt, FaHammer } from "react-icons/fa";
 import PropTypes from "prop-types";
+=======
+import { FaHome, FaRedditAlien, FaSignInAlt, FaHammer, FaPaintRoller } from "react-icons/fa";
+>>>>>>> 2360c1c (feat(front): HomePage initialization)
 
 function NavigationBar({isAdmin}) {
   return (
